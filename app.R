1f5c4f4d037a5eb97f2badeb8168f91e693e1293
Pawel Cwiek <altodato@outlook.com>
2020-11-07 22:55:04 +0100

    cases per 100,000, limits in PL

--- app.R
+++ app.R
@@ -105,6 +105,7 @@ ui <- fluidPage(
                     checkboxInput("checkSmooth", label = "Smoothed conditional mean", value = FALSE),
                     checkboxInput("checkConfidenceInterval", label = "Show confidence interval", value = FALSE),
                     checkboxInput("casespm", label = "New cases per million", value = FALSE),
+                    checkboxInput("casesp100", label = "New cases per 100,000", value = FALSE),
                     checkboxGroupInput(
                       "checkCountries",
                       label = h4("Select countries"),
@@ -166,6 +167,7 @@ server <- function(input, output, session) {
       mutate(
         Date = as.Date(dateRep),
         cases_per_million = round(cases / (popData2019 / 1000000), 0),
+        cases_per_100k = round(cases / (popData2019 / 100000), 0),
         Country = countriesAndTerritories
       ) %>%
       select(Date,
@@ -173,7 +175,8 @@ server <- function(input, output, session) {
              cases,
              deaths,
              popData2019,
-             cases_per_million) %>%
+             cases_per_million,
+             cases_per_100k) %>%
       filter(complete.cases(.)) %>%
       arrange(Date) %>%
       group_by(Country) %>%
@@ -181,7 +184,8 @@ server <- function(input, output, session) {
         Date,
         cases,
         case_sum = cumsum(cases),
-        cases_per_million
+        cases_per_million,
+        cases_per_100k
       ) %>% 
       filter(Date >= input$sdate & Date <= input$edate) %>% 
       ungroup()
@@ -195,6 +199,24 @@ server <- function(input, output, session) {
           geom_line() +
           ylab("New cases per million per day") +
           scale_y_continuous(breaks = round(seq(0, max(dp()$cases_per_million), by = 50), 0), limits = c(0,NA))
+    } else if(input$casesp100) {
+      p <- ggplot(data = dp(),
+                  aes(x = Date, y = cases_per_100k, color = Country)) +
+        geom_line() +
+        geom_hline(yintercept = 70, color = "black", linetype = "dashed") +
+        annotate("text", x = input$sdate + 2 , y = 70, vjust = -1, color = "black",
+                 label = "Poland - lockdown limit, 7 days average") +
+        geom_hline(yintercept = 50, color = "red", linetype = "dashed") +
+        annotate("text", x = input$sdate + 2 , y = 50, vjust = -1, color = "red",
+                 label = "Poland - red zone upper limit, 7 days average") +
+        geom_hline(yintercept = 25, color = "red", linetype = "dashed") +
+        annotate("text", x = input$sdate + 2 , y = 25, vjust = -1, color = "red",
+                 label = "Poland - red zone lower limit, 7 days average") +
+        geom_hline(yintercept = 10, color = "#ffcc00", linetype = "dashed") +
+        annotate("text", x = input$sdate + 2 , y = 10, vjust = -1, color = "#ffcc00",
+                 label = "Poland - yellow zone limit, 7 days average") +
+        ylab("New cases per 100,000 per day") +
+        scale_y_continuous(breaks = round(seq(0, max(dp()$cases_per_100k), by = 10), 0), limits = c(0,NA))
     } else {
       if(max(dp()$cases) - min(dp()$cases) > 4000 & max(dp()$cases) - min(dp()$cases) < 10000) {
         by_ticks = 1000
@@ -233,14 +255,15 @@ server <- function(input, output, session) {
   
   output$covidTable <- renderDataTable({
       dt <- dp()
-      names(dt) <- c("Country", "Date", "New cases", "Sum of all cases per country", "New cases per million")
+      names(dt) <- c("Country", "Date", "New cases", "Sum of all cases per country", 
+                     "New cases per million", "New cases per 100,000")
       dt[,5] <- round(dt[,5], 2)
+      dt[,6] <- round(dt[,6], 2)
       dt
       }
   )
     
   output$covid_hc_plot <- renderHighchart({
-   
     if(input$casespm) {
       dhc <- dp() %>%
         group_by(Country) %>%
@@ -251,6 +274,16 @@ server <- function(input, output, session) {
           append(list(name = x), y)
         })}
       y_text <- "New cases per million per day"      
+    } else if(input$casesp100) {
+      dhc <- dp() %>%
+        group_by(Country) %>%
+        do(dhc = list(
+          data = list_parse2(data.frame(.$Date, .$cases_per_100k))
+        )) %>%
+        {map2(.$Country, .$dhc, function(x, y){
+          append(list(name = x), y)
+        })}
+      y_text <- "New cases per 100,000 per day"      
     } else {
       dhc <- dp() %>%
         group_by(Country) %>%
@@ -263,14 +296,38 @@ server <- function(input, output, session) {
       y_text <- "New cases"
     }
   
-    highchart() %>% 
-      hc_plotOptions(series = list(marker = list(enabled = FALSE))) %>% 
-      hc_add_series_list(dhc) %>%
-      hc_xAxis(categories = unique(dp()$Date)) %>% 
-      hc_yAxis(title = list(text = y_text)) %>% 
-      hc_tooltip(table = TRUE,
-                 sort = TRUE) %>% 
-      hc_colors(dcolors)
+    if(input$casesp100){
+      highchart() %>% 
+        hc_plotOptions(series = list(marker = list(enabled = FALSE))) %>% 
+        hc_add_series_list(dhc) %>%
+        hc_xAxis(categories = unique(dp()$Date)) %>% 
+        hc_yAxis(title = list(text = y_text),
+                 plotLines = list(list(value = 70, color = "black", width = 2, dashStyle = "shortdash",
+                                       label = list(text = "Poland - lockdown limit, 7 days average",
+                                                    style = list(color = "black", fontWeight = "bold"))),
+                                  list(value = 50, color = "red", width = 2, dashStyle = "shortdash",
+                                       label = list(text = "Poland - red zone upper limit, 7 days average",
+                                                    style = list(color = "red", fontWeight = "bold"))),
+                                  list(value = 25, color = "red", width = 2, dashStyle = "shortdash",
+                                       label = list(text = "Poland - red zone lower limit, 7 days average",
+                                                    style = list(color = "red", fontWeight = "bold"))),
+                                  list(value = 10, color = "#ffcc00", width = 2, dashStyle = "shortdash",
+                                       label = list(text = "Poland - yellow zone limit, 7 days average",
+                                                    style = list(color = "#ffcc00", fontWeight = "bold"))))
+                 ) %>% 
+        hc_tooltip(table = TRUE,
+                   sort = TRUE) %>% 
+        hc_colors(dcolors)
+    } else {
+      highchart() %>% 
+        hc_plotOptions(series = list(marker = list(enabled = FALSE))) %>% 
+        hc_add_series_list(dhc) %>%
+        hc_xAxis(categories = unique(dp()$Date)) %>% 
+        hc_yAxis(title = list(text = y_text)) %>% 
+        hc_tooltip(table = TRUE,
+                   sort = TRUE) %>% 
+        hc_colors(dcolors)
+    }
     
   })
 
@@ -309,18 +366,37 @@ server <- function(input, output, session) {
       shinyjs::disable("checkSmooth")
       shinyjs::disable("checkConfidenceInterval")
       shinyjs::enable("casespm")
+      shinyjs::enable("casesp100")
     } else if(input$tabs == "ECDPC ggplot") {
       shinyjs::enable("checkSmooth")
       shinyjs::enable("checkConfidenceInterval")
       shinyjs::enable("casespm")
+      shinyjs::enable("casesp100")
     } else if(input$tabs == "ECDPC Highcharts bar plot"){
+      updateCheckboxInput(session, inputId = "checkSmooth", value = FALSE)
+      updateCheckboxInput(session, inputId = "checkConfidenceInterval", value = FALSE)
+      updateCheckboxInput(session, inputId = "casespm", value = FALSE)
+      updateCheckboxInput(session, inputId = "casesp100", value = FALSE)
       shinyjs::disable("checkSmooth")
       shinyjs::disable("checkConfidenceInterval")
       shinyjs::disable("casespm")
+      shinyjs::disable("casesp100")
     }
   })
 
-  # Disable buttons at start
+  observeEvent(eventExpr = input$casespm, {
+    if(input$casespm) {
+      updateCheckboxInput(session, inputId = "casesp100", value = FALSE)
+    }
+  })
+
+  observeEvent(eventExpr = input$casesp100, {
+    if(input$casesp100) {
+      updateCheckboxInput(session, inputId = "casespm", value = FALSE)
+    }
+  })
+  
+    # Disable buttons at start
   shinyjs::disable("checkSmooth")
   shinyjs::disable("checkConfidenceInterval")
   
